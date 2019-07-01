4a29fb16eb827302babb80b8ba7b20c41a3b2e31
cmattey <cmattey@dons.usfca.edu>
2019-07-01 01:20:12 -0700

    Added recetn swell value to Beach info

--- surf_app/beaches.py
+++ surf_app/beaches.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint, url_for, redirect, g, render_template, flash, request, session
 )
 
+from surf_app import app
+import requests
 from surf_app.auth import login_required
 
 bp = Blueprint('beaches',__name__)
@@ -9,15 +11,34 @@ bp = Blueprint('beaches',__name__)
 @bp.route('/beaches')
 @login_required
 def beaches_home():
-    beach_list = get_beaches()
-    return render_template('beaches/beaches_home.html',beaches=beach_list)
+    beach_info = get_beach_info()
+    return render_template('beaches/beaches_home.html',beaches=beach_info)
 
 
-def get_beaches():
+def get_beach_info():
     """
     Returns a list of Beach ID and name, the IDs are according to MSW's web API.
     """
     beaches = [{'beach_id': 255, 'beach_name':'Linda Mar / Pacifica'},
                 {'beach_id': 819, 'beach_name':'Ocean Beach'}]
+
+
+    for beach in beaches:
+        beach_url = app.config['MSW_API_URL'].format(key=app.config['MSW_API_KEY'],spot_id=beach['beach_id'])
+        msw_response = requests.get(beach_url)
+
+        try:
+            msw_response.raise_for_status()
+        except Exception as exc:
+            print("There was a problem with MagicSeaWeed API:",(exc))
+
+        beach_info = msw_response.json()
+
+        parameters = ["localTimestamp", "swell.minBreakingHeight",
+                    "swell.maxBreakingHeight","swell.probability",
+                    "swell.components[primary].compassDirection",
+                    "wind.speed","wind.compassDirection","condition.temperature"]
+
+        beach["beach_info"] = beach_info[0]
 
     return beaches
