db6541154f6eab7cbfae5e745ac37c38f80cf78b
cmattey <cmattey@dons.usfca.edu>
2019-08-17 01:02:38 -0700

    Moved aync follow function to js static directory, updates js code

--- surf_app/static/js/follow_async.js
+++ surf_app/static/js/follow_async.js
@@ -1,11 +1,9 @@
-<script>
-        function follow_async(sourceElem, destElem, follow_username) {
-            // $(destElem).html('<img src="{{ url_for('static', filename='loading.gif') }}">');
-            $.get("/user/follow_async/{}".format(follow_username)
-            ).done(function(response) {
-                $(destElem).text(response['text'])
-            }).fail(function() {
-                $(destElem).text("Error: Could not contact server.");
-            });
-        }
-  </script>
+function follow_async(sourceElem, destElem, follow_username) {
+    // $(destElem).html('<img src="{{ url_for('static', filename='loading.gif') }}">');
+    $.get("/user/follow_async/"+follow_username
+    ).done(function(response) {
+        $(destElem).text(response['text'])
+    }).fail(function() {
+        $(destElem).text("Error: Could not contact server.");
+    });
+}
